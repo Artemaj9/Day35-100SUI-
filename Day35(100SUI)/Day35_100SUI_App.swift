e3c9a1e8feda7f80eaebe858ddcee2bb624cae62
Artem <moiseev@physics.msu.ru>
2023-08-04 22:45:38 +0300

    Initial Commit

---
+++ Day35(100SUI)/Day35_100SUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Day35_100SUI_App.swift
+//  Day35(100SUI)
+//
+//  Created by Artem on 04.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Day35_100SUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
